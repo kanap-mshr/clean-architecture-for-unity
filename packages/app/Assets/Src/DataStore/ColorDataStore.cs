935278ab8b6160affa23d96d3687bd95b83f9fed
kanap-mshr <76883092+kanap-mshr@users.noreply.github.com>
2022-01-29 22:37:08 +0900

    更新

--- packages/app/Assets/Src/DataStore/ColorDataStore.cs
+++ packages/app/Assets/Src/DataStore/ColorDataStore.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorDataStore : MonoBehaviour
+public class ColorDataStore
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  // Entity
+  private ColorEntity _entity;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+  // ストレージからロード
+  public ColorEntity Load() {
+    if (_entity != null) {
+      return _entity;
     }
+    var r = PlayerPrefs.HasKey("r") ? PlayerPrefs.GeFloat("r") : 1f;
+    var g = PlayerPrefs.HasKey("g") ? PlayerPrefs.GeFloat("g") : 1f;
+    var b = PlayerPrefs.HasKey("b") ? PlayerPrefs.GeFloat("b") : 1f;
+    _entity = new ColorEntity(r, g, b);
+    return _entity;
+  }
+
+  // ストレージに保存
+  public void Save(ColorEntity entity) {
+    _entity = entity;
+    PlayerPrefs.SeFloat("r", entity.R);
+    PlayerPrefs.SeFloat("g", entity.G);
+    PlayerPrefs.SeFloat("b", entity.B);
+    PlayerPrefs.Save();
+  }
 }
