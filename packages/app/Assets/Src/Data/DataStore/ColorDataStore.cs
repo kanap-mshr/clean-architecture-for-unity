1da9f4b3e668f7cce63af4e57f1d943ac576f8ff
kanap-mshr <76883092+kanap-mshr@users.noreply.github.com>
2022-01-31 23:35:28 +0900

    更新

--- packages/app/Assets/Src/Data/DataStore/ColorDataStore.cs
+++ packages/app/Assets/Src/Data/DataStore/ColorDataStore.cs
@@ -1,27 +1,27 @@
-// using UnityEngine;
-//
-// public class ColorDataStore {
-//   // Entity
-//   private ColorEntity _entity;
-//
-//   // ストレージからロード
-//   public ColorEntity Load() {
-//     if (_entity != null) {
-//       return _entity;
-//     }
-//     var r = PlayerPrefs.HasKey("r") ? PlayerPrefs.GeFloat("r") : 1f;
-//     var g = PlayerPrefs.HasKey("g") ? PlayerPrefs.GeFloat("g") : 1f;
-//     var b = PlayerPrefs.HasKey("b") ? PlayerPrefs.GeFloat("b") : 1f;
-//     _entity = new ColorEntity(r, g, b);
-//     return _entity;
-//   }
-//
-//   // ストレージに保存
-//   public void Save(ColorEntity entity) {
-//     _entity = entity;
-//     PlayerPrefs.SeFloat("r", entity.R);
-//     PlayerPrefs.SeFloat("g", entity.G);
-//     PlayerPrefs.SeFloat("b", entity.B);
-//     PlayerPrefs.Save();
-//   }
-// }
+using UnityEngine;
+
+public class ColorDataStore {
+  // Entity
+  private ColorEntity _entity;
+
+  // ストレージからロード
+  public ColorEntity Load() {
+    if (_entity != null) {
+      return _entity;
+    }
+    float r = PlayerPrefs.HasKey("r") ? PlayerPrefs.GetFloat("r") : 1f;
+    float g = PlayerPrefs.HasKey("g") ? PlayerPrefs.GetFloat("g") : 1f;
+    float b = PlayerPrefs.HasKey("b") ? PlayerPrefs.GetFloat("b") : 1f;
+    _entity = new ColorEntity(r, g, b);
+    return _entity;
+  }
+
+  // ストレージに保存
+  public void Save(ColorEntity entity) {
+    _entity = entity;
+    PlayerPrefs.SetFloat("r", entity.R);
+    PlayerPrefs.SetFloat("g", entity.G);
+    PlayerPrefs.SetFloat("b", entity.B);
+    PlayerPrefs.Save();
+  }
+}
